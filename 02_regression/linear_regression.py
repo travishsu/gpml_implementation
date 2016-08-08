ae60aa33ec2fc113d530a7de2732d45d53417b88
Yichung Hsu <fov223@gmail.com>
2016-08-08 13:19:21 +0800

    description

--- 02_regression/linear_regression.py
+++ 02_regression/linear_regression.py
@@ -1,33 +1,34 @@
-import numpy as np
-from numpy import exp, dot, linspace, power, eye
+from numpy import exp, dot, linspace, power, eye, array, sin, cos
 from numpy.linalg import norm, solve, inv, svd
 from matplotlib.pylab import plot, figure, show, scatter
 
 def covariance( x_1, x_2 ):
     # Square Exponential Kernel
-    return exp( - power(norm(x_1 - x_2), 3) * 0.5 )
+    return exp( - power(norm(x_1 - x_2), 2  ) * 0.5 )
 
 def feature(x):
-    #return np.array( [1, x, x*x/10, x*x*x/100, x*x*x*x/1000, x*x*x*x*x/10000] )
-    return np.array( [1, np.sin(x), np.cos(x)] )
+    return array( [ x ] )
+    #return array( [1, sin(x), cos(x)] )
 
 def kerMat(xlist, ylist):
     ker = []
     for x in xlist:
         ker.append( [covariance( feature(x), feature(y) ) for y in ylist] )
-    return np.array(ker)
+    return array(ker)
 
-x = np.array( [1,2,3,4,5] )
-y = np.sin(x)
+# Generating Training Data
+x = array( [1,2,3,4,5] )
+y = sin(x)
 
-# prediction
-x_star = linspace(1, 5, 1000)
-
-sigma = 0.01
+# Modeling: Parametes estimation
+sigma = 0.0001
 w = solve( kerMat(x, x) + sigma*sigma*eye(x.size), y )
 
+# Prediction
+x_star = linspace(1, 5, 1000)
 y_star = dot( kerMat(x_star, x), w )
 
+# Plotting
 figure()
 plot(x_star, y_star)
 plot(x_star, np.sin(x_star))
