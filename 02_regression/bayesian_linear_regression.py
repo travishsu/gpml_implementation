c0b3788da576ac66ab92c6d6562615c10c6d7aa3
Yichung Hsu <fov223@gmail.com>
2016-08-10 09:57:29 +0800

    ..

--- 02_regression/bayesian_linear_regression.py
+++ 02_regression/bayesian_linear_regression.py
@@ -1,13 +1,14 @@
-from numpy import exp, dot, linspace, power, eye, array, sin, cos
-from numpy.linalg import norm, solve, inv, svd
-from matplotlib.pylab import plot, figure, show, scatter
+from numpy import exp, dot, linspace, power, eye, array, sin, cos, pi, diag, log, sum
+from numpy.linalg import norm, solve, inv, svd, cholesky
+from matplotlib.pylab import plt, scatter
 
 def covariance( x_1, x_2 ):
-    # Square Exponential Kernel
-    return exp( - power(norm(x_1 - x_2), 2  ) * 0.5 )
+    # Squared Exponential covariance function
+    l = 10 # length-scale parameter
+    return exp( - power(norm(x_1 - x_2)/l, 2  ) * 0.5 )
 
 def feature(x):
-    return array( [ x ] )
+    return array( [ 1, (x-1)*(x-5)/2, x] )
     #return array( [1, sin(x), cos(x)] )
 
 def kerMat(xlist, ylist):
@@ -16,22 +17,30 @@ def kerMat(xlist, ylist):
         ker.append( [covariance( feature(x), feature(y) ) for y in ylist] )
     return array(ker)
 
+def fit(x, y, noise_level):
+    w = solve( kerMat(x, x) + noise_level * eye(x.size), y )
+    log_marginal_likelihood = - dot(y, w)/2 - sum( log( diag( cholesky(kerMat(x, x) + noise_level * eye(x.size)) ) ) ) - x.size*log(2*pi)/2
+    return w, fit_score
+
 # Generating Training Data
-x = array( [1,2,3,4,5] )
+x = linspace(0, 2*pi, 5)
 y = sin(x)
 
-# Modeling: Parametes estimation
-sigma = 0.0001
-w = solve( kerMat(x, x) + sigma*sigma*eye(x.size), y )
-
+# Modeling: Parameters estimation
+noise_level = 0.0001
+w = solve( kerMat(x, x) + noise_level * eye(x.size), y )
+log_marginal_likelihood = - dot(y, w)/2 - sum( log( diag( cholesky(kerMat(x, x) + noise_level * eye(x.size)) ) ) ) - x.size*log(2*pi)/2
+print log_marginal_likelihood
 # Prediction
-x_star = linspace(1, 5, 1000)
+x_star = linspace(0, 2*pi, 1000)
 y_star = dot( kerMat(x_star, x), w )
+var_star = kerMat( x_star, x_star ) - dot( kerMat( x_star, x ), solve( kerMat( x, x ), kerMat( x, x_star ) ) )
 
 # Plotting
-figure()
-plot(x_star, y_star)
-plot(x_star, sin(x_star))
+plt.plot(x_star, y_star)
+plt.plot(x_star, y_star+100*diag(var_star))
+plt.plot(x_star, y_star-100*diag(var_star))
+plt.plot(x_star, sin(x_star))
 for i in range(x.size):
-    scatter( x[i], y[i] )
-show()
+    plt.scatter( x[i], y[i] )
+plt.show()
