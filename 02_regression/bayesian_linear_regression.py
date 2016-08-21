c7dffc6b652cc6f59f564d63b24b72791b9d3957
Yichung Hsu <fov223@gmail.com>
2016-08-21 10:31:38 +0800

    class created but have problem in var calculation

--- 02_regression/bayesian_linear_regression.py
+++ 02_regression/bayesian_linear_regression.py
@@ -2,32 +2,45 @@ from numpy import exp, dot, linspace, power, eye, array, sin, cos, pi, diag, log
 from numpy.linalg import norm, solve, inv, svd, cholesky
 from matplotlib.pylab import plt, scatter
 
-def covariance( x_1, x_2 ):
-    # Squared Exponential covariance function
-    l = 0.064 # length-scale parameter
-    return exp( - power(norm(x_1 - x_2)/l, 2  ) * 0.5 )
+class GaussianProcessRegression:
+    def __init__( self, X, y, noise_level ):
+        self.X = X
+        self.y = y
+        self.noise_level = noise_level
 
-def feature(x):
-    return array( [ 1, (x-1)*(x-5)/2, x] )
-    #return array( [1, sin(x), cos(x)] )
+        self.x_feature = array([self.feature(xi) for xi in self.X])
+        self.L = cholesky( self.kerMat(self.X, self.X) + self.noise_level * eye(self.X.size) )
+        self.w = solve( self.L.T, solve(  self.L,  self.y ) )
+        self.log_marginal_likelihood = - dot(self.y, self.w)/2 - sum( log( diag( self.L ) ) ) - self.X.size*log(2*pi)/2
 
-def kerMat(xlist, ylist):
-    ker = []
-    for x in xlist:
-        ker.append( [covariance( feature(x), feature(y) ) for y in ylist] )
-    return array(ker)
+    def covariance( self, x_1, x_2 ):
+        # Squared Exponential covariance function
+        l = 1 # length-scale parameter
+        return exp( - power(norm(x_1 - x_2)/l, 2  ) * 0.5 )
 
-def fit(x, y, noise_level):
-    L = cholesky( kerMat(x, x) + noise_level * eye(x.size) )
-    w = solve( L.T, solve( L, y ) )
-    log_marginal_likelihood = - dot(y, w)/2 - sum( log( diag( L ) ) ) - x.size*log(2*pi)/2
-    return w, fit_score
+    def feature( self, x ):
+        return array( [ 1, (x-1)*(x-5)/2, x] )
+        #return array( [1, sin(x), cos(x)] )
 
-def predict(x, X, w, L):
-    f = dot( kerMat(x, X), w )
-    v = solve(L, kerMat(x, X))
-    var = kerMat( x, x ) - dot(v.T, v)
-    return f, var
+    def kerMat( self, xlist, ylist ):
+        ker = []
+        for x in xlist:
+            ker.append( [self.covariance( self.feature(x), self.feature(y) ) for y in ylist] )
+        return array(ker)
+
+    #def fit(self, x, y, noise_level):
+    #    L = cholesky( kerMat(x, x) + noise_level * eye(x.size) )
+    #    w = solve( L.T, solve( L, y ) )
+    #    log_marginal_likelihood = - dot(y, w)/2 - sum( log( diag( L ) ) ) - x.size*log(2*pi)/2
+    #    return w, fit_score
+
+    def predict(self, xtest):
+        self.xtest = xtest;
+        self.f = dot( self.kerMat(self.xtest, self.X), self.w )
+        #v = solve(self.L, self.kerMat(self.X, self.xtest))
+        #print v.shape
+        #self.var = self.kerMat( self.xtest, self.X ) - dot(v.T, v)
+        return self.f#, self.var
 
 # Generating Training Data
 x = linspace(0, 2*pi, 50)
@@ -35,13 +48,10 @@ y = sin(x)
 
 # Modeling: Parameters estimation
 noise_level = 0.01
-w = solve( kerMat(x, x) + noise_level * eye(x.size), y )
-log_marginal_likelihood = - dot(y, w)/2 - sum( log( diag( cholesky(kerMat(x, x) + noise_level * eye(x.size)) ) ) ) - x.size*log(2*pi)/2
-print log_marginal_likelihood
+model = GaussianProcessRegression(x, y, noise_level)
 # Prediction
 x_star = linspace(0, 2*pi, 1000)
-y_star = dot( kerMat(x_star, x), w )
-var_star = kerMat( x_star, x_star ) - dot( kerMat( x_star, x ), solve( kerMat( x, x ), kerMat( x, x_star ) ) )
+y_star = model.predict(x_star)
 
 # Plotting
 plt.plot(x_star, y_star)
